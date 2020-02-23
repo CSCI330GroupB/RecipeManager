09e4e553ca0a06e96a4fb75c9cc268021ce430bf
Nick Cody <nicholas.james.cody@gmail.com>
2020-02-23 10:57:40 -0500

    Added a main class

---
+++ RecipeManager/src/RecipeManagerMain.java
@@ -0,0 +1,19 @@
+/**
+ * @author Group B; Nick Cody, Ben Hestad, Jaydeon Jacobs
+ * 
+ * @section CSCI 330
+ * 
+ * @date 2/23/2019
+ * 
+ * @assignment Implementation I
+ * 
+ * @description This program demonstrates a unit test of the addInstruction method of the Recipe class
+ */
+public class RecipeManagerMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
